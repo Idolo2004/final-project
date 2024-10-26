cea601e5d352b41ad9a39acfd7f588b04a8f2d21
Idolo2004 <ahmed9mohammed9900@gmail.com>
2024-10-26 09:27:49 +0000

    Sat, Oct 26, 2024, 9:27 AM +00:00

--- utility.py
+++ utility.py
@@ -1,1 +1,11 @@
-from 
+from rembg import remove
+from PIL import Image
+import io
+
+with open("webapp.jpeg", 'rb') as input_file:
+     input_image = input_file.read()
+
+output_image = remove(input_image)
+
+with open("logo.jpg", "wb") as output_file:
+    output_file.write(output_image)
